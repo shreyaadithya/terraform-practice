f6804112904b84e813454542682127cdab116efa
Ubuntu <ubuntu@ip-172-31-27-168.ec2.internal>
2025-09-08 07:50:25 +0000

    2nd commit to check remotebackend

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "aws" {
 resource "aws_key_pair" "test_key" {
     public_key = file("/home/ubuntu/.ssh/id_ed25519.pub")
 }
-resource "aws_security_group" "ssh-access_tf" {
-    name        = "ssh-access_tf"
+resource "aws_security_group" "ssh-access_tf_05" {
+    name        = "ssh-access_tf_05"
     description = "Allow ssh accesss from internet"
     ingress {
         from_port   = 22
@@ -38,9 +38,9 @@ resource "aws_security_group" "ssh-access_tf" {
 }
 resource "aws_instance" "test_tf_creation" {
     ami = "ami-0360c520857e3138f"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     key_name = aws_key_pair.test_key.id
-    vpc_security_group_ids = [aws_security_group.ssh-access_tf.id]
+    vpc_security_group_ids = [aws_security_group.ssh-access_tf_05.id]
     tags = {
         name = "terraform_tf_state"
         Description = "terraform practice on ubuntu"
